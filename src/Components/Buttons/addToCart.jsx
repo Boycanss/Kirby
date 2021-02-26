fe68d88029ae3669a5d75f7b734788ab52006981
Bawang cs <boy.simanjuntak49@gmail.com>
2021-02-26 11:46:24 +0700

    fixed addToCart btn

--- src/Components/Buttons/addToCart.jsx
+++ src/Components/Buttons/addToCart.jsx
@@ -14,22 +14,31 @@ const AddToCart = ({ productID, size, qty }) => {
         setLoadingNotif(true);
         console.log({ id }, { size }, { qty });
 
-        commerce.products.retrieve(id)
-            .then((prod) => {
-                console.log(prod)
-                const { variants } = prod;
-                const sz = variants[1].options;
-                const szID = sz[size].id;
-                commerce.cart.add(szID, qty)
-                    .then((res) => {
-                        // console.log(res);
-                        setLoadingNotif(false);
-                        setNotif(true);
-                        document.addEventListener('click', () => {
-                            setNotif(false)
-                        });
-                    })
-            })
+        commerce.cart.add(szID, qty)
+        .then((res) => {
+            // console.log(res);
+            setLoadingNotif(false);
+            setNotif(true);
+            document.addEventListener('click', () => {
+                setNotif(false)
+            });
+        })
+        // commerce.products.retrieve(id)
+        //     .then((prod) => {
+        //         console.log(prod)
+        //         const { variants } = prod;
+        //         const sz = variants[1].options;
+        //         const szID = sz[size].id;
+        //         commerce.cart.add(szID, qty)
+        //             .then((res) => {
+        //                 // console.log(res);
+        //                 setLoadingNotif(false);
+        //                 setNotif(true);
+        //                 document.addEventListener('click', () => {
+        //                     setNotif(false)
+        //                 });
+        //             })
+        //     })
     }
 
     let notify = notif === true ? <CartUpdated /> : null;
